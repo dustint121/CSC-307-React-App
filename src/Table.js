be5d85ae7b4e818b75faaf6769b030f94347cca4
dustint121 <58173584+dustint121@users.noreply.github.com>
2022-04-01 13:07:55 -0700

    Added Remove Button for each row

--- src/Table.js
+++ src/Table.js
@@ -10,6 +10,7 @@ function TableHeader()  {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -18,32 +19,39 @@ function TableHeader()  {
 
 
 
-  function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>         
-          <td>{row.job}</td>
-        </tr>
-      );
-     }
-    );
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-          {rows}
-         </tbody>
-     );
-  }
+      <tr key={index}>
+        <td>{row.name}</td>         
+        <td>{row.job}</td>
+        <td>
+           <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+    );
+    }
+  );
+  return (
+      <tbody>
+        {rows}
+      </tbody>
+    );
+}
   
   
-  function Table(props) {
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={props.characterData} />
-        </table>
-      );
-  }
+
+
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
+}
+  
+  
 
 
 export default Table;
