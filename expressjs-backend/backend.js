28e7356676513e17fda4ab0a2f0e189ac134351d
dustint121 <58173584+dustint121@users.noreply.github.com>
2022-04-11 11:26:35 -0700

    Finished Task 1 and 5 of Part 6; Implemented 201 code and Id column for post-function

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -93,9 +93,14 @@ function findUserById(id) {
 //Part 6
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
+    userToAdd['id'] = randomId();
     addUser(userToAdd);
-    res.status(200).end();
+    res.status(201).send(userToAdd);
 });
+
+function randomId(){
+    return Math.random().toString(36).substring(2, 8);
+}
 
 function addUser(user){
     users['users_list'].push(user);
