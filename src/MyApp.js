3b76048e4d3b9ae605cde8e38e6e91d76af77cf0
dustint121 <58173584+dustint121@users.noreply.github.com>
2022-04-11 11:19:03 -0700

    Finished Part 5 and connecting the frontend and backend; Input to frontend will show up in backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -36,12 +36,24 @@ function MyApp() {
 
 
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 200)
+        setCharacters([...characters, person] );
+    });
   }
     
     
-
+  async function makePostCall(person){
+    try {
+        const response = await axios.post('http://localhost:5000/users', person);
+        return response;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+  }
 
 return (
   <div className="container">
